500a1e92ed2162ae1204c740a2b058ec9744fdb4
Mateus Ribeiro <mateus.ribeiro@techsavvy.com.br>
2022-09-28 11:24:59 -0300

    :construction: added Carga model

--- app/Models/Carga.ts
+++ app/Models/Carga.ts
@@ -4,5 +4,41 @@ export default class Carga extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  
+  @column()
+  public previsao: string
+
+  @column()
+  public end_entrega: string
+
+  @column()
+  public end_retirada: string
+
+  @column()
+  public tipo: string
+
+  @column()
+  public obs: string
+
+  @column()
+  public dest_nome: string
+
+  @column()
+  public status_rastreio: string
+
+  @column()
+  public dimensoes: string
+
+  @column()
+  public volume: string
+
+  @column()
+  public veiculo_id: number
+
+  @column()
+  public rota_id: number
+
+  @column()
+  public motorista_id: number
+
+  //TODO: relacionamento das FK
 }
